06bde65816d72e2af709a14865a1419563742f61
dgencay <deniz.gencay@nucleo.com.tr>
2022-06-20 02:01:13 +0300

    Paging 3 lib config

--- app/src/main/java/com/example/borutoapp/data/local/dao/HeroRemoteKeysDao.kt
+++ app/src/main/java/com/example/borutoapp/data/local/dao/HeroRemoteKeysDao.kt
@@ -4,16 +4,16 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.borutoapp.domain.model.HeroRemoteKey
+import com.example.borutoapp.domain.model.HeroRemoteKeys
 
 @Dao
-interface HeroRemoteKeyDao {
+interface HeroRemoteKeysDao {
 
-    @Query("SELECT * FROM hero_remote_key_table WHERE id=:id")
-    suspend fun getRemoteKey(id: Int): HeroRemoteKey?
+    @Query("SELECT * FROM hero_remote_key_table WHERE id = :heroId")
+    suspend fun getRemoteKeys(heroId: Int): HeroRemoteKeys?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addAllRemoteKeys(heroRemoteKeys: List<HeroRemoteKey>)
+    suspend fun addAllRemoteKeys(heroRemoteKeys: List<HeroRemoteKeys>)
 
     @Query("DELETE FROM hero_remote_key_table")
     suspend fun deleteAllRemoteKeys()
